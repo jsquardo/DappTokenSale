47e88b4f7f08298a89bc5d03e99a29be980afc9d
Johnny Squardov <johnny@johnmichael.dev>
2021-03-18 10:55:24 -0400

    Working on DappToken

---
+++ contracts/DappToken.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.2;
+
+contract DappToken {
+  // Constructor
+  // Set the total number of tokens
+  // Read the total number of tokens
+  function DappToken() public {
+    totalSupply = 1000000;
+  }
+}
